7d991808623a6b9fe1f3491578330f4e62d48e94
ngocvo2511 <ngocvo12345ssssaaaa@gmail.com>
2024-11-08 21:58:21 +0700

    Them Assets, tao class Player, Position, Direction

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,35 @@
+﻿namespace ChessLogic
+{
+    public class Direction
+    {
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction West = new Direction(0, -1);
+        public readonly static Direction NorthEast = new Direction(-1, 1);
+        public readonly static Direction NorthWest = new Direction(-1, -1);
+        public readonly static Direction SouthEast = new Direction(1, 1);
+        public readonly static Direction SouthWest = new Direction(1, -1);
+
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(dir1.RowDelta + dir2.RowDelta, dir1.ColumnDelta + dir2.ColumnDelta);
+        }
+        public static Direction operator *(int scale, Direction dir)
+        {
+            return new Direction(scale * dir.RowDelta, scale * dir.ColumnDelta);
+        }
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta);
+        }
+    }
+}
