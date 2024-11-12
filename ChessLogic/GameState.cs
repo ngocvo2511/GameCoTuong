e4febad798218000fe3b522fe433d3b711e735a2
chun2005 <hoangquangnghia123@gmail.com>
2024-11-12 09:37:31 +0700

    undo move

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -9,11 +9,15 @@ namespace ChessLogic
     public class GameState
     {
         public Board Board { get; }
+        public bool isAI {  get;}
+        public Stack<Move> Moved { get; set; }
         public Player CurrentPlayer { get; private set; }
-        public GameState(Player player, Board board)
+        public GameState(Player player, Board board, bool isAI=false)
         {
             CurrentPlayer = player;
             Board = board;
+            this.isAI = isAI;
+            Moved=new Stack<Move>();
         }
 
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
@@ -30,7 +34,26 @@ namespace ChessLogic
         public void MakeMove(Move move)
         {
             move.Execute(Board);
+            Moved.Push(move);
             CurrentPlayer = CurrentPlayer.Opponent();
+            if(isAI == true && CurrentPlayer == Player.Black)
+            {
+                //AI move
+            }
+        }
+        public void UndoMove(bool testMove=false)
+        {
+            if (!Moved.Any()) return;
+            int i;
+            if (testMove == false && isAI == true && CurrentPlayer == Player.Red) i = 2;
+            else i = 1;
+            for(int j = 0; j < i; j++)
+            {
+                Move move = Moved.Pop();
+                Move undoMove = new NormalMove(move.ToPos, move.FromPos);
+                undoMove.Execute(Board);
+                CurrentPlayer = CurrentPlayer.Opponent();
+            }
         }
     }
 }
