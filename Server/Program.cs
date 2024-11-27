1174e6ea42e95977e09514bafecc7cfc6df66a33
ngocvo2511 <ngocvo12345ssssaaaa@gmail.com>
2024-11-27 15:45:52 +0700

    add signalR

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,5 @@
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
+app.MapHub<ChessHub>("/chessHub");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
