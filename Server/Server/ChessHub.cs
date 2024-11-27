66827c0226d82b359306e8f3912a822c6bb4af03
Võ Xuân Ngọc <ngocvo12345ssssaaaa@gmail.com>
2024-11-27 14:49:49 +0700

    Create Server

---
+++ Server/Server/ChessHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server
+{
+    public class ChessHub : Hub
+    {
+       
+        public async Task Click(int a, int b, int c, int d)
+        {
+            await Clients.All.SendAsync("ClickAtPoint", a, b, c, d);
+        }
+    }
+
+ 
+}
