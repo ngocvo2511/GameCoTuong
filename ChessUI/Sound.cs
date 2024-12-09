6cb6cb6235f3b2736d05d10d071e52c6b3c95131
ngocvo2511 <ngocvo12345ssssaaaa@gmail.com>
2024-12-10 00:09:44 +0700

    create class sound

---
+++ ChessUI/Sound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ChessUI
+{
+    public class Sound
+    {
+        private static MediaPlayer buttonClickSound = new MediaPlayer();
+        private static MediaPlayer gameOverSound = new MediaPlayer();
+        private static MediaPlayer moveSound = new MediaPlayer();
+
+    }
+}
