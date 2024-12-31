8ee1dbbd7065704383a00e923bfc421054b30a4c
huyenbtp <huyenbtp2005@gmail.com>
2025-01-01 04:20:00 +0700

    bổ sung luật chơi, thêm SearchingMatchMenu

--- ChessUI/Menus/GameOverMenu.xaml.cs
+++ ChessUI/Menus/GameOverMenu.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using ChessLogic;
 using ChessLogic.GameStates.GameState;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Schema;
 
 namespace ChessUI.Menus
 {
@@ -64,7 +51,7 @@ namespace ChessUI.Menus
         {
             return reason switch
             {
-                EndReason.Stalemate => $"{PlayerString(currentPlayer)} không thể di chuyển",
+                EndReason.Stalemate => $"{PlayerString(currentPlayer)} hết nước đi",
                 EndReason.Checkmate => $"{PlayerString(currentPlayer)} bị chiếu bí",
                 EndReason.InsufficientMaterial => "Hòa vì thiếu quân",
                 EndReason.FiftyMoveRule => "Hòa vì 50 nước không ăn quân",
---
+++ ChessUI/Menus/SearchingMatchMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI.Menus
+{
+    /// <summary>
+    /// Interaction logic for SearchingMatchMenu.xaml
+    /// </summary>
+    public partial class SearchingMatchMenu : UserControl
+    {
+        public SearchingMatchMenu()
+        {
+            InitializeComponent();
+        }
+        public static readonly RoutedEvent BackButtonClickedEvent = EventManager.RegisterRoutedEvent(
+            "BackButtonClicked",
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(SearchingMatchMenu)
+            );
+        public event RoutedEventHandler BackButtonClicked
+        {
+            add { AddHandler(BackButtonClickedEvent, value); }
+            remove { RemoveHandler(BackButtonClickedEvent, value); }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(BackButtonClickedEvent));
+        }
+    }
+}
